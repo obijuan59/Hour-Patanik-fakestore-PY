44e0c93a08498967ec4f8f17a309d1f5ff87c00b
obijuan59 <Vothoul483@gmail.com>
2025-08-28 21:43:34 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask_mail import Mail, Message
+import json
+import requests
+from datetime import date
+from dotenv import load_dotenv
+import os
+# import iod
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+# Load environment variables (if using .env)
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = 'your_flask_secret_key'
+
+# Mail configuration (optional)
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "vothoul456@gmail.com"
+app.config['MAIL_PASSWORD'] = "vvwbwlzxqlguyzhn"
+app.config['MAIL_DEFAULT_SENDER'] = ('ShowMe', app.config['MAIL_USERNAME'])
+mail = Mail(app)
+
+# --- Upload Folder ---
+# This will create a folder named "uploads" inside "static" automatically if it doesn't exist
+UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'uploads')
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+mail = Mail(app)
+
+# Telegram config
+BOT_TOKEN = '7205157576:AAGNGn_KUjEHXhyiM3ukgqTCgsQdL2oDLZg'
+CHAT_ID = '@Invoice_notify_blyat_bot'
+
+import route
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# @app.get('/detail/<int:pro_id>')
+# def detail(pro_id):
+#     return f"{pro_id}"
